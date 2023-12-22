4c6463f92a6c0e5b1d46af55ed50476c5a6a00e4
Eileen Ilsemann <eileenilsemanni@gmail.com>
2023-12-22 11:33:16 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <div>
+      {" "}
+      <HelloWorld />{" "}
+    </div>
+  );
+}
+
+function HelloWorld() {
+  return <h1>Hello World</h1>;
 }
